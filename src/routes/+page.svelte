d0836a1219b24e57105ec8ae743f4308a4d5640b
Azlothe <bbqian7@gmail.com>
2024-12-02 22:39:16 -0800

    feat: toggle to play screen on upload

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,22 @@
 <script lang="ts">
 	import UploadAudio from '$lib/components/upload/UploadAudio.svelte';
+	import PlayAudio from '$lib/components/play/PlayAudio.svelte';
+
+	let uploadedAudio: File | null = null;
+
+	$: if (uploadedAudio) {
+		console.log(uploadedAudio);
+	}
 </script>
 
 <div class="h-screen w-screen">
-	<div class="h-full p-12">
-		<UploadAudio />
-	</div>
+	{#if uploadedAudio}
+		<div class="h-full w-full bg-black">
+			<PlayAudio file={uploadedAudio} />
+		</div>
+	{:else}
+		<div class="h-full p-12">
+			<UploadAudio onFileSelect={(file) => (uploadedAudio = file)} />
+		</div>
+	{/if}
 </div>
