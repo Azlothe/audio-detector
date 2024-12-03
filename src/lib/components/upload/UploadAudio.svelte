1ae30f849da88e191ceb0137ec14f18e72f670a7
Azlothe <bbqian7@gmail.com>
2024-12-02 21:10:19 -0800

    feat: make upload audio page

---
+++ src/lib/components/upload/UploadAudio.svelte
@@ -0,0 +1,85 @@
+<div class="box flex h-full w-full flex-col items-center justify-center space-y-12">
+	<span class="flex items-center space-x-5 text-gray-500">
+		<span class="flex h-12 w-12 items-center justify-center">
+			<svg viewBox="0 0 500 500" class="h-full w-full">
+				<circle
+					cx="249.9"
+					cy="250.4"
+					r="204.7"
+					stroke="#808080"
+					stroke-width="22"
+					stroke-miterlimit="10"
+					fill="none"
+				/>
+				<g>
+					<line
+						fill="none"
+						stroke="#808080"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-miterlimit="10"
+						stroke-width="22"
+						x1="250"
+						x2="250"
+						y1="123"
+						y2="372"
+					/>
+					<line
+						fill="none"
+						stroke="#808080"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-miterlimit="10"
+						stroke-width="22"
+						x1="374"
+						x2="126"
+						y1="247"
+						y2="247"
+					/>
+				</g>
+			</svg>
+		</span>
+		<h1>Upload Audio</h1>
+	</span>
+
+	<span class="flex items-center space-x-5 text-gray-500">
+		<h2>Accepted file formats: <code>.mp3</code> <code>.m4a</code></h2>
+	</span>
+</div>
+
+<style>
+	.box {
+		--b: 1rem;
+		--c: rgba(128, 128, 128, 0.2);
+		--w: 5rem;
+		--r: 1.5rem;
+		padding: var(--b);
+		position: relative;
+	}
+
+	.box::before {
+		content: '';
+		position: absolute;
+		inset: 0;
+		background: var(--c);
+		padding: var(--b);
+		border-radius: var(--r);
+
+		mask:
+			linear-gradient(0deg, #000 calc(2 * var(--b)), #0000 0) 50% var(--b) /
+				calc(100% - 2 * var(--w)) 100% repeat-y,
+			linear-gradient(-90deg, #000 calc(2 * var(--b)), #0000 0) var(--b) 50% / 100%
+				calc(100% - 2 * var(--w)) repeat-x,
+			linear-gradient(#000 0 0) content-box,
+			linear-gradient(#000 0 0);
+		-webkit-mask:
+			linear-gradient(0deg, #000 calc(2 * var(--b)), #0000 0) 50% var(--b) /
+				calc(100% - 2 * var(--w)) 100% repeat-y,
+			linear-gradient(-90deg, #000 calc(2 * var(--b)), #0000 0) var(--b) 50%/100%
+				calc(100% - 2 * var(--w)) repeat-x,
+			linear-gradient(#000 0 0) content-box,
+			linear-gradient(#000 0 0);
+		-webkit-mask-composite: destination-out;
+		mask-composite: exclude;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import UploadAudio from '$lib/components/upload/UploadAudio.svelte';
+</script>
+
+<div class="h-screen w-screen">
+	<div class="h-full p-12">
+		<UploadAudio />
+	</div>
+</div>
