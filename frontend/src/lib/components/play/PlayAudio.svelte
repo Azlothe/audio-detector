d3cc2c1c5458c6e73fda8b7f82e9da7d3ace05d9
Azlothe <bbqian7@gmail.com>
2024-12-03 23:03:04 -0800

    feat: add play button

--- frontend/src/lib/components/play/PlayAudio.svelte
+++ frontend/src/lib/components/play/PlayAudio.svelte
@@ -43,18 +43,6 @@
 	</AudioFrequency>
 
 	<div
-		class="absolute left-0 top-0 flex h-full w-full flex-col items-center justify-center rounded-xl"
-	>
-		<CircleBarAudioVisualizer
-			audio={currentlyPlaying}
-			startHue={120}
-			endHue={120}
-			rotate={2}
-			{analysisType}
-		/>
-	</div>
-
-	<div
 		class="absolute left-0 top-1/4 flex h-1/2 w-full flex-col items-center justify-center rounded-xl border border-white/15 p-4 px-24 opacity-50 blur-lg"
 	>
 		<BarAudioVisualizer
@@ -67,12 +55,39 @@
 		/>
 	</div>
 
-	<button
-		type="button"
-		on:click={startAudio}
-		class="absolute left-1/2 top-1/2 rounded-full px-3 py-2 text-sm font-semibold focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-amber-500 {paused
-			? 'border border-stone-500/20 bg-stone-500/10 text-stone-500 hover:bg-stone-500/20'
-			: 'border border-amber-500/20 bg-amber-500/10 text-amber-500 hover:bg-amber-500/20'}"
-		>{paused ? 'Start ' : 'Pause '}music</button
+	<div
+		class="absolute left-0 top-0 flex h-full w-full flex-col items-center justify-center rounded-xl"
 	>
+		<CircleBarAudioVisualizer
+			audio={currentlyPlaying}
+			startHue={120}
+			endHue={120}
+			rotate={2}
+			{analysisType}
+		/>
+		{#if paused}
+			<button
+				class="absolute ml-4 h-32 w-32 rounded-full outline-none"
+				on:click={startAudio}
+				aria-label="button"
+			>
+				<svg
+					fill="white"
+					version="1.1"
+					id="Layer_1"
+					xmlns="http://www.w3.org/2000/svg"
+					xmlns:xlink="http://www.w3.org/1999/xlink"
+					viewBox="0 0 330 330"
+					xml:space="preserve"
+				>
+					<path
+						id="XMLID_308_"
+						d="M37.728,328.12c2.266,1.256,4.77,1.88,7.272,1.88c2.763,0,5.522-0.763,7.95-2.28l240-149.999
+c4.386-2.741,7.05-7.548,7.05-12.72c0-5.172-2.664-9.979-7.05-12.72L52.95,2.28c-4.625-2.891-10.453-3.043-15.222-0.4
+C32.959,4.524,30,9.547,30,15v300C30,320.453,32.959,325.476,37.728,328.12z"
+					/>
+				</svg></button
+			>
+		{/if}
+	</div>
 </div>
