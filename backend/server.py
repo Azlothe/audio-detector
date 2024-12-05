98cb9be747e4f014526456b0bec50f122909d69e
Azlothe <bbqian7@gmail.com>
2024-12-04 23:32:58 -0800

    fix: add CORS middleware to FastAPI server

--- backend/server.py
+++ backend/server.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI
+from routes import stream_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(stream_router)
 
 @app.get("/")
 def read_root():
